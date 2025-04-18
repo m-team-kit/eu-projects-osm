df73c97d178081f4ab7058b62956d2adf58c5e91
vykozlov <valentin.kozlov@gmail.com>
2025-04-18 15:34:33 +0200

    feat: plotting only necessary region

--- eu-projects.py
+++ eu-projects.py
@@ -1,28 +1,28 @@
 import geopandas as gpd
+import logging
 import matplotlib.pyplot as plt
 import contextily as ctx
-from shapely.geometry import Point, LineString
+from shapely.geometry import Point, LineString, box
+
+logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG) # 0: Not set, 10: DEBUG, 20: INFO, 30: WARNING, 40: ERROR, 50: CRITICAL
+
+# create console handler and set level to debug
+#ch = logging.StreamHandler()
+#ch.setLevel(logging.DEBUG)
+#logger.addHandler(ch)
 
 # Load world map data
-#world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 world = gpd.read_file("data/ne_50m_admin_0_countries.shp")
 
 # Define a bounding box for Europe in EPSG:4326 (lon/lat)
-europe_bounds = {
-    "minx": -25,   # westernmost (e.g., Portugal/Iceland)
-    "maxx": 45,    # easternmost (e.g., Slovakia/Ukraine)
-    "miny": 34,    # southernmost (e.g., Mediterranean)
-    "maxy": 72     # northernmost (e.g., Scandinavia)
-}
-
-# Keep only countries within the bounding box
-world_europe = world.cx[
-    europe_bounds["minx"]:europe_bounds["maxx"],
-    europe_bounds["miny"]:europe_bounds["maxy"]
-]
-
-# Then reproject as needed
-world_europe = world_europe.to_crs(epsg=3857)
+#  "minx": -25,   # westernmost (e.g., Portugal/Iceland)
+#  "maxx": 45,    # easternmost (e.g., Slovakia/Ukraine)
+#  "miny": 34,    # southernmost (e.g., Mediterranean)
+#  "maxy": 72     # northernmost (e.g., Scandinavia)
+#  box(minx, miny, maxx, maxy)
+europe_bbox = box(-25, 32, 40, 60)
 
 # Filter for countries you want to show (e.g., in Europe)
 countries = world[world['NAME'].isin(['Spain', 'Germany', 'France', 
@@ -47,6 +47,7 @@ karlsruhe_coords = (8.4037, 49.0069)  # lon, lat
 
 # Define AI4EOSC destination cities
 ai4eosc = {
+    'name': 'AI4EOSC',
     'destinations': { 'Valencia': (-0.3763, 39.4699),
                       'Santander': (-3.8044, 43.4623),
                       'Lisboa': (-9.1393, 38.7223),
@@ -67,8 +68,9 @@ ai4eosc = {
 }
 
 # Define iMagine-AI destination cities
-shift_same_city=0.3
+shift_same_city=0.26
 imagine_ai = {
+    'name': 'iMagine',
     'destinations': { 'Valencia': (-0.3763, 39.4699+shift_same_city),
                       'Santander': (-3.8044, 43.4623+shift_same_city),
                       'Lisboa': (-9.1393, 38.7223+shift_same_city),
@@ -125,36 +127,45 @@ def draw_connections(project):
     city_gdf.plot(ax=ax, color=marker['color'], markersize=marker['size'], zorder=3, alpha=marker['alpha'])
 
 
-
-# Reproject all to Web Mercator for contextily
-countries = countries.to_crs(epsg=3857)
-# Reproject only Europe as needed
-world_europe = world_europe.to_crs(epsg=3857)
-
 # Plotting
 fig, ax = plt.subplots(figsize=(12, 12), dpi=200) # Bigger figure and higher DPI
 
-# Select Spain from the countries GeoDataFrame
+
+# Clip the world data according to bbox for Europe
+countries = countries.to_crs(epsg=4326)
+countries = gpd.clip(countries, europe_bbox)
+# Reproject only Europe as needed
+countries = countries.to_crs(epsg=3857)
+logger.info("Reprojected Europe")
+
+# Select e.g. Spain from the countries GeoDataFrame
 # Fill Spain with semi-transparent dark cyan
 #spain = world[world['NAME'] == 'Spain'].to_crs(epsg=3857)
 #spain.plot(ax=ax, color='darkcyan', alpha=0.5, edgecolor='darkcyan')
 
 # Draw country boundaries
-#countries.boundary.plot(ax=ax, color='darkcyan')
-countries.boundary.plot(ax=ax, color='dimgray', linewidth=0.5, linestyle='--', antialiased=True, alpha=0.5)
+# vkoz@250418: borders are artificially cut by Europe bbox..
+countries.boundary.plot(ax=ax, color='dimgray', linewidth=0.5, linestyle='--', antialiased=True, alpha=0.7)
+logger.info("Added country borders")
 
 draw_connections(imagine_ai)
+logger.info("Added project: iMagine AI")
+
 draw_connections(ai4eosc)
+logger.info("Added project: AI4EOSC")
 
 # Plot city names (we combine cities from all projects, because there can be duplicates otherwise)
-cities = {  **ai4eosc['destinations'],
-            **imagine_ai['destinations']
+cities = {  **imagine_ai['destinations'],
+            **ai4eosc['destinations']
          }
-print(cities)
+logger.debug(cities)
+
 cities_gdf = city_geo(cities)
 cities_gdf = cities_gdf.to_crs(epsg=3857)
 for x, y, label in zip(cities_gdf.geometry.x, cities_gdf.geometry.y, cities_gdf['name']):
     ax.text(x + 10000, y + 10000, label, fontsize=4)
+
+logger.info("Added project cities")
 
 #for x, y, label in zip(city_gdf.geometry.x, city_gdf.geometry.y, city_gdf['name']):
 #    ax.text(x + 5000, y + 5000, label, fontsize=12, fontweight='bold', 
@@ -162,11 +173,14 @@ for x, y, label in zip(cities_gdf.geometry.x, cities_gdf.geometry.y, cities_gdf[
 #            bbox=dict(facecolor='white', alpha=0.5, edgecolor='none', boxstyle="round,pad=0.3"))
 
 # Add OpenStreetMap basemap at a higher zoom level
-ctx.add_basemap(ax, source=ctx.providers.OpenStreetMap.Mapnik, zoom=5)
+ctx.add_basemap(ax, source=ctx.providers.OpenStreetMap.Mapnik, zoom=7)
 #ctx.add_basemap(ax, source=ctx.providers.CartoDB.Positron, zoom=4)
 
 plt.title("EU projects connections")
 plt.axis('off')
 plt.tight_layout()
 plt.savefig("map_high_res.pdf", dpi=600, bbox_inches='tight')  # 300+ DPI = print quality
+logger.info("Saved PDF")
+plt.savefig("map_high_res.jpg", dpi=300, bbox_inches='tight')  # 300+ DPI = print quality
+logger.info("Saved JPEG")
 plt.show()
